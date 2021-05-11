450f9bf7e13da4e459d83066cdc166ab742fbb85
Conrad Scherb <csch666@aucklanduni.ac.nz>
2021-05-12 01:12:45 +1200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _313_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
